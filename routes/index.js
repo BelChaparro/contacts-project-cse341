959604736e95310a038c7ed935869b647359cc33
BelChaparro <a.belenchaparro@gmail.com>
2024-01-13 23:41:49 -0300

    Added a default message for the home page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get("/", (req, res) => {
+    res.send("This is the home page");
+});
+
 router.use("/contacts", require("./contacts"));
 
 module.exports = router;
