2213f5c0698a0b805cc05a3f8b864f5b94af6a81
BelChaparro <a.belenchaparro@gmail.com>
2024-01-19 15:06:24 -0300

    Adding all endpoints for contacts routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,6 +6,12 @@ const usersController = require("../controllers/contacts");
 router.get("/", usersController.getAll);
 // GET request for a single contact:
 router.get("/:id", usersController.getSingle);
+// POST request for adding a new contact:
+router.post("/", usersController.createContact);
+// PUT request for udpating an existing contact:
+router.put("/:id", usersController.updateContact);
+// DELETE request for deleting an existing contact:
+router.delete("/:id", usersController.deleteContact);
 
 // Export router:
 module.exports = router;
