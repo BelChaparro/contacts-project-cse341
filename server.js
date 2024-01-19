a9695d313a8f8bb7dfe87e159c28f24d6b47ccca
BelChaparro <a.belenchaparro@gmail.com>
2024-01-19 15:41:39 -0300

    changed bodyParser to appear before accesing the routes

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// When "/" go to ./routes/index.js
-app.use("/", require("./routes"));
 // To be able to ready the body from the request object when creating a new contact:
 app.use(bodyParser.json());
+// When "/" go to ./routes/index.js
+app.use("/", require("./routes"));
+
 
 mongodb.initDb((err) => {
   if (err) {
