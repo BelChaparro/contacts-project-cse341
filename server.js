d3e928ce61ba7a1f86a30e002ff301cd2c3d77b6
BelChaparro <a.belenchaparro@gmail.com>
2024-01-27 03:40:13 -0300

    moved body-parser to the top of app.use

--- server.js
+++ server.js
@@ -5,19 +5,25 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allows cross-origin
+app
+  // To be able to ready the body from the request object when creating a new contact:
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allows cross-origin
     res.setHeader('Access-Control-Allow-Headers', // Allows any header
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', // Allow all methods from API
-  'GET, POST, PUT, DELETE, OPTIONS');
-  next();
+    res.setHeader('Access-Control-Allow-Methods', // Allow all methods from API
+    'GET, POST, PUT, DELETE, OPTIONS');
+    next();
 });
 
-// To be able to ready the body from the request object when creating a new contact:
-app.use(bodyParser.json());
 // When "/" go to ./routes/index.js
 app.use("/", require("./routes"));
+
+// "Catch all" error handling:
+process.on("uncaughtException", (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception on origin: ${origin}`);
+});
 
 
 mongodb.initDb((err) => {
