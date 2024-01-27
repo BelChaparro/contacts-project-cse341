a9a02b1d6cb4927c4fb0c1a34ea1a94c18ae6327
BelChaparro <a.belenchaparro@gmail.com>
2024-01-27 03:42:30 -0300

    Included validation to routes /post and /put

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/contacts");
+//const validation = require('../middleware/validator.js')
+const { userValidationRules, validate } = require('../middleware/validator');
 
 // GET request for all contacts:
 router.get("/", usersController.getAll);
 // GET request for a single contact:
 router.get("/:id", usersController.getSingle);
-// POST request for adding a new contact:
-router.post("/", usersController.createContact);
-// PUT request for udpating an existing contact:
-router.put("/:id", usersController.updateContact);
+// POST request for adding a new contact & validation:
+router.post("/", userValidationRules(), validate, usersController.createContact);
+// PUT request for udpating an existing contact & validation:
+router.put("/:id", userValidationRules(), validate, usersController.updateContact);
 // DELETE request for deleting an existing contact:
 router.delete("/:id", usersController.deleteContact);
 
